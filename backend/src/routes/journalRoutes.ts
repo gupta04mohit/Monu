40f8bb813702db1525e7368eac133ed06d161520
mohitgupta__04 <gupta04mohit@gmail.com>
2026-07-05 21:26:24 +0530

    Delete backend directory

--- backend/src/routes/journalRoutes.ts
+++
@@ -1,8 +0,0 @@
-import express from 'express';
-import { saveJournalEntry } from '../controllers/journalController';
-
-const router = express.Router();
-
-router.post('/', saveJournalEntry);
-
-export default router;
